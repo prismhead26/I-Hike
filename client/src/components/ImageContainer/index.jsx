d33a7c0ea80909b43dfd49e973379c45861467b2
Aiden Wahed <prismhead26@gmail.com>
2024-06-03 17:00:44 -0600

    image rounded

--- client/src/components/ImageContainer/index.jsx
+++ client/src/components/ImageContainer/index.jsx
@@ -5,7 +5,7 @@ const Image = () => {
     <div className="container">
     <div className="row justify-content-center">
       <div className="col-md-8 col-lg-6">
-        <img src={Mountain} alt="Rocky Mountains" className="img-fluid" />
+        <img src={Mountain} alt="Rocky Mountains" className="img-fluid rounded" />
       </div>
     </div>
   </div>
