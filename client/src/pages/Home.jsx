2ff922af9e0a20d00b3b03876137034fa334e60d
jacksonjjones <jacksonjjones1@gmail.com>
2024-06-03 16:48:16 -0500

    fixed clear button overlapping edge of page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Input } from "@mui/base";
 import { Button } from "@mui/base";
 import { fetchWeather } from "../utils/API/openWeatherMap";
 import TrailsMap from "../utils/API/googleMaps";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   const [city, setCity] = useState("");
@@ -15,19 +17,19 @@ const Home = () => {
     lng: -105.280243,
   });
 
-      // maps through city name and caps the first letter of each work
+  // Maps through city name and caps the first letter of each word
   const cityName = city.split(" ").map((word) => {
     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
   }).join(" ");
 
-  // save city to local storage and set city to local storage
+  // Save city to local storage and set city to local storage
   useEffect(() => {
     const savedCity = localStorage.getItem("city");
     if (savedCity) {
       setCity(savedCity);
     }
 
-    // fetch weather data if city is saved
+    // Fetch weather data if city is saved
     if (savedCity && savedCity !== "") {
       fetchWeather(
         savedCity,
@@ -38,17 +40,16 @@ const Home = () => {
         setGearRecommendation
       );
     }
+  }, []);
 
-  }, [])
-
-  // save city to local storage
+  // Save city to local storage
   useEffect(() => {
     localStorage.setItem("city", cityName);
-  }, [cityName])
+  }, [cityName]);
 
   return (
     <main>
-      <div className="container my-3">
+      <div className="container my-4">
         <div className="input-container mb-3">
           <Input
             value={cityName}
@@ -78,16 +79,18 @@ const Home = () => {
           >
             Search
           </Button>
-          {/* add a clear search button which sets city to '' */}
+          {/* Add a clear search button which sets city to '' */}
           <Button
             variant="contained"
             color="primary"
-            onClick={() => {setCity("")
+            onClick={() => {
+              setCity("");
               window.location.reload();
             }}
-            className="btn btn-outline-secondary search-button">
-            Clear Search
-            </Button>
+            className="btn btn-outline-secondary clear-button ml-1"
+          >
+            <FontAwesomeIcon icon={faArrowsRotate} size="1x" />
+          </Button>
         </div>
         {loading ? (
           <div>Loading...</div>
@@ -110,8 +113,7 @@ const Home = () => {
               </div>
               <section>Gear Recommendation: {gearRecommendation}</section>
               <div className="map-container">
-                <TrailsMap
-                  coords={newCoords} />
+                <TrailsMap coords={newCoords} />
               </div>
             </div>
           )
